acf35725b36be338c3aa650ab9d422b7c299c5d8
Regina28-m <regina-railan2013@yandex.ru>
2023-02-09 09:27:54 +0300

    commit

--- src/js/Validator.js
+++ src/js/Validator.js
@@ -1,16 +1,12 @@
-export default class Validator {
+export default classValidator{
   constructor(name) {
     this.name = name;
   }
 
-  validateUsername() {
-    const checkSymbol = /^[a-zA-Z0-9-_]+$/;
-    const checkOrder = /^[A-Z][A-Z0-9-_]+[A-Z]$/i;
-    if (!checkSymbol.test(this.name)) {
-      throw new Error('Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)');
-    }
-    if (!checkOrder.test(this.name) || this.name.search(/\d{4,}/)) {
-      throw new Error('Имя не должно содержать подряд более трёх цифр, а также начинаться и заканчиваться цифрами, символами подчёркивания или тире.');
-    }
+  validateUserName() {
+    return /[a-zA-Z]+[\d_-]*/.test(this.name)
+     && /^[^\d_-]/.test(this.name)
+     && !/\d{4,}/.test(this.name)
+    && /[^\d_-]$/.test(this.name);
   }
 }
